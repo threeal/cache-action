0350b48445d1349277ccf31284dbbff578b395b0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-15 21:39:05 +0700

    feat: modify action to get caches information

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-import { getInput, logError } from "gha-utils";
-import { mkdirRecursive } from "./mkdir.js";
+import { logError, logInfo } from "gha-utils";
+import { sendCacheApiRequest } from "./api.js";
+
+interface Caches {
+  totalCount: number;
+  artifactCaches: {
+    id: number;
+    cacheKey: string;
+  }[];
+}
 
 try {
-  const path = getInput("path");
-  mkdirRecursive(path);
+  logInfo("Getting caches information...");
+  const [status, caches] = await sendCacheApiRequest<Caches>("caches", {
+    method: "GET",
+  });
+  if (status !== 200) {
+    throw new Error(`Failed to get caches information: ${status}`);
+  }
+
+  logInfo(`Found ${caches.totalCount} caches:`);
+  for (const cache of caches.artifactCaches) {
+    logInfo(`- ${cache.id} ${cache.cacheKey}`);
+  }
 } catch (err) {
   logError(err);
   process.exit(1);

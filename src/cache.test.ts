ebe70eb1ba8385cfe16dde9dbc83e9a3e3dbf798
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-18 23:15:32 +0700

    test: refactor mocked declaration in `cache.test.ts` file

--- src/cache.test.ts
+++ src/cache.test.ts
@@ -1,65 +1,67 @@
-import type http from "node:http";
 import { jest } from "@jest/globals";
 
+const createRequest = jest.fn();
+const handleErrorResponse = jest.fn();
+const handleJsonResponse = jest.fn();
+const sendJsonRequest = jest.fn();
+
 jest.unstable_mockModule("./api.js", () => ({
-  createRequest: jest.fn(),
-  handleErrorResponse: jest.fn(),
-  handleJsonResponse: jest.fn(),
-  sendJsonRequest: jest.fn(),
+  createRequest,
+  handleErrorResponse,
+  handleJsonResponse,
+  sendJsonRequest,
 }));
 
 describe("reserve caches", () => {
+  beforeAll(() => {
+    createRequest.mockImplementation((resourcePath, options) => {
+      expect(resourcePath).toBe("caches");
+      expect(options).toEqual({ method: "POST" });
+      return "some request";
+    });
+  });
+
   it("should reserve a cache", async () => {
-    const { createRequest, handleJsonResponse, sendJsonRequest } = await import(
-      "./api.js"
-    );
     const { reserveCache } = await import("./cache.js");
 
-    jest
-      .mocked(createRequest)
-      .mockReturnValue("some request" as unknown as http.ClientRequest);
-
-    jest.mocked(handleJsonResponse).mockResolvedValue({ cacheId: 32 });
-
-    jest.mocked(sendJsonRequest).mockResolvedValue({
-      statusCode: 201,
-    } as unknown as http.IncomingMessage);
-
-    const cacheId = await reserveCache("some-key", "some-version", 1024);
-
-    expect(createRequest).toHaveBeenCalledTimes(1);
-    expect(createRequest).toHaveBeenCalledWith("caches", { method: "POST" });
-
-    expect(sendJsonRequest).toHaveBeenCalledTimes(1);
-    expect(sendJsonRequest).toHaveBeenCalledWith("some request", {
-      key: "some-key",
-      version: "some-version",
-      cacheSize: 1024,
+    sendJsonRequest.mockImplementation(async (req, data) => {
+      expect(req).toBe("some request");
+      expect(data).toEqual({
+        key: "some-key",
+        version: "some-version",
+        cacheSize: 1024,
+      });
+      return { statusCode: 201 };
     });
 
-    expect(handleJsonResponse).toHaveBeenCalledTimes(1);
-    expect(handleJsonResponse).toHaveBeenCalledWith({ statusCode: 201 });
+    handleJsonResponse.mockImplementation(async (res) => {
+      expect(res).toEqual({ statusCode: 201 });
+      return { cacheId: 32 };
+    });
 
+    const cacheId = await reserveCache("some-key", "some-version", 1024);
     expect(cacheId).toBe(32);
   });
 
   it("should fail to reserve a cache", async () => {
-    const { createRequest, handleErrorResponse, sendJsonRequest } =
-      await import("./api.js");
     const { reserveCache } = await import("./cache.js");
 
-    jest
-      .mocked(createRequest)
-      .mockReturnValue("some request" as unknown as http.ClientRequest);
+    sendJsonRequest.mockImplementation(async (req, data) => {
+      expect(req).toBe("some request");
+      expect(data).toEqual({
+        key: "some-key",
+        version: "some-version",
+        cacheSize: 1024,
+      });
+      return { statusCode: 500 };
+    });
 
-    jest.mocked(handleErrorResponse).mockResolvedValue(new Error("some error"));
+    handleErrorResponse.mockImplementation(async (res) => {
+      expect(res).toEqual({ statusCode: 500 });
+      return new Error("some error");
+    });
 
-    jest.mocked(sendJsonRequest).mockResolvedValue({
-      statusCode: 500,
-    } as unknown as http.IncomingMessage);
-
-    await expect(
-      reserveCache("some-key", "some-version", 1024),
-    ).rejects.toThrow("some error");
+    const prom = reserveCache("some-key", "some-version", 1024);
+    await expect(prom).rejects.toThrow("some error");
   });
 });

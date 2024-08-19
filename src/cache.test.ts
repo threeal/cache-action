32a041d6adb5cdc45cd90880c36a197a8d4f2f84
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-19 17:08:53 +0700

    feat: modify action to save cache (#16)
    * feat: add `uploadCache` function
    
    * test: add test for `uploadCache` function
    
    * feat: modify action to upload file to cache
    
    * feat: add `commitCache` function
    
    * test: add test for `commitCache` function
    
    * feat: modify action to commit cache after upload

--- src/cache.test.ts
+++ src/cache.test.ts
@@ -3,13 +3,17 @@ import { jest } from "@jest/globals";
 const createRequest = jest.fn();
 const handleErrorResponse = jest.fn();
 const handleJsonResponse = jest.fn();
+const handleResponse = jest.fn();
 const sendJsonRequest = jest.fn();
+const sendStreamRequest = jest.fn();
 
 jest.unstable_mockModule("./api.js", () => ({
   createRequest,
   handleErrorResponse,
   handleJsonResponse,
+  handleResponse,
   sendJsonRequest,
+  sendStreamRequest,
 }));
 
 describe("reserve caches", () => {
@@ -62,6 +66,98 @@ describe("reserve caches", () => {
     });
 
     const prom = reserveCache("some-key", "some-version", 1024);
+    await expect(prom).rejects.toThrow("some error");
+  });
+});
+
+describe("upload files to caches", () => {
+  beforeAll(() => {
+    createRequest.mockImplementation((resourcePath, options) => {
+      expect(resourcePath).toBe("caches/32");
+      expect(options).toEqual({ method: "PATCH" });
+      return "some request";
+    });
+  });
+
+  it("should upload a file to a cache", async () => {
+    const { uploadCache } = await import("./cache.js");
+
+    sendStreamRequest.mockImplementation(async (req, bin, start, end) => {
+      expect(req).toBe("some request");
+      expect(bin).toEqual("some file");
+      expect(start).toBe(0);
+      expect(end).toBe(1024);
+      return { statusCode: 204 };
+    });
+
+    handleResponse.mockImplementation(async (res) => {
+      expect(res).toEqual({ statusCode: 204 });
+    });
+
+    await uploadCache(32, "some file" as any, 1024);
+  });
+
+  it("should fail to upload a file to a cache", async () => {
+    const { uploadCache } = await import("./cache.js");
+
+    sendStreamRequest.mockImplementation(async (req, bin, start, end) => {
+      expect(req).toBe("some request");
+      expect(bin).toEqual("some file");
+      expect(start).toBe(0);
+      expect(end).toBe(1024);
+      return { statusCode: 500 };
+    });
+
+    handleErrorResponse.mockImplementation(async (res) => {
+      expect(res).toEqual({ statusCode: 500 });
+      return new Error("some error");
+    });
+
+    const prom = uploadCache(32, "some file" as any, 1024);
+    await expect(prom).rejects.toThrow("some error");
+  });
+});
+
+describe("commit caches", () => {
+  beforeAll(() => {
+    createRequest.mockImplementation((resourcePath, options) => {
+      expect(resourcePath).toBe("caches/32");
+      expect(options).toEqual({ method: "POST" });
+      return "some request";
+    });
+  });
+
+  it("should commit a cache", async () => {
+    const { commitCache } = await import("./cache.js");
+
+    sendJsonRequest.mockImplementation(async (req, data) => {
+      expect(req).toBe("some request");
+      expect(data).toEqual({ size: 1024 });
+      return { statusCode: 204 };
+    });
+
+    handleResponse.mockImplementation(async (res) => {
+      expect(res).toEqual({ statusCode: 204 });
+    });
+
+    await commitCache(32, 1024);
+  });
+
+  it("should fail to commit a cache", async () => {
+    const { commitCache } = await import("./cache.js");
+
+    sendJsonRequest.mockImplementation(async (req, data) => {
+      expect(req).toBe("some request");
+      expect(data).toEqual({ size: 1024 });
+      return { statusCode: 500 };
+    });
+
+    handleErrorResponse.mockImplementation(async (res) => {
+      expect(res).toEqual({ statusCode: 500 });
+      return new Error("some error");
+    });
+
+    const prom = commitCache(32, 1024);
     await expect(prom).rejects.toThrow("some error");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
 import { getInput, logError, logInfo } from "gha-utils";
-import { reserveCache } from "./cache.js";
+import fs from "node:fs";
+import { commitCache, reserveCache, uploadCache } from "./cache.js";
 
 try {
+  const filePath = getInput("file");
+  const fileSize = fs.statSync(filePath).size;
+
   logInfo("Reserving cache...");
   const cacheId = await reserveCache(
     getInput("key"),
     getInput("version"),
-    parseInt(getInput("size"), 10),
+    fileSize,
   );
-  logInfo(`Reserved cache with id: ${cacheId}`);
+  logInfo(`Cache reserved with id: ${cacheId}`);
+
+  logInfo("Uploading cache...");
+  const file = fs.createReadStream(filePath, {
+    fd: fs.openSync(filePath, "r"),
+    autoClose: false,
+    start: 0,
+    end: fileSize,
+  });
+  await uploadCache(cacheId, file, fileSize);
+  logInfo("Cache uploaded");
+
+  logInfo("Commiting cache...");
+  await commitCache(cacheId, fileSize);
+  logInfo("Cache committed");
 } catch (err) {
   logError(err);
   process.exit(1);

352d3fc611be57f08cb0275643d86ba252f470b1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-21 21:37:56 +0700

    feat: utilize `saveCache` function in the action

--- dist/index.mjs
+++ dist/index.mjs
@@ -228,6 +228,26 @@ async function restoreCache(key, version, filePath) {
     await downloadFile(cache.archiveLocation, filePath);
     return true;
 }
+/**
+ * Saves a file to the cache using the specified key and version.
+ *
+ * @param key - The cache key.
+ * @param version - The cache version.
+ * @param filePath - The path of the file to be saved.
+ * @returns A promise that resolves when the file has been successfully saved.
+ */
+async function saveCache(key, version, filePath) {
+    const fileSize = fs.statSync(filePath).size;
+    const cacheId = await reserveCache(key, version, fileSize);
+    const file = fs.createReadStream(filePath, {
+        fd: fs.openSync(filePath, "r"),
+        autoClose: false,
+        start: 0,
+        end: fileSize,
+    });
+    await uploadCache(cacheId, file, fileSize);
+    await commitCache(cacheId, fileSize);
+}
 
 try {
     const key = getInput("key");
@@ -239,22 +259,8 @@ try {
         process.exit(0);
     }
     logInfo("Cache does not exist, saving...");
-    const fileSize = fs.statSync(filePath).size;
-    logInfo("Reserving cache...");
-    const cacheId = await reserveCache(key, version, fileSize);
-    logInfo(`Cache reserved with id: ${cacheId}`);
-    logInfo("Uploading cache...");
-    const file = fs.createReadStream(filePath, {
-        fd: fs.openSync(filePath, "r"),
-        autoClose: false,
-        start: 0,
-        end: fileSize,
-    });
-    await uploadCache(cacheId, file, fileSize);
-    logInfo("Cache uploaded");
-    logInfo("Commiting cache...");
-    await commitCache(cacheId, fileSize);
-    logInfo("Cache committed");
+    await saveCache(key, version, filePath);
+    logInfo("Cache successfully saved");
 }
 catch (err) {
     logError(err);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { getInput, logError, logInfo } from "gha-utils";
-import fs from "node:fs";
-import { commitCache, reserveCache, uploadCache } from "./api/cache.js";
-import { restoreCache } from "./cache.js";
+import { restoreCache, saveCache } from "./cache.js";
 
 try {
   const key = getInput("key");
@@ -13,27 +11,10 @@ try {
     logInfo("Cache successfully restored");
     process.exit(0);
   }
+
   logInfo("Cache does not exist, saving...");
-
-  const fileSize = fs.statSync(filePath).size;
-
-  logInfo("Reserving cache...");
-  const cacheId = await reserveCache(key, version, fileSize);
-  logInfo(`Cache reserved with id: ${cacheId}`);
-
-  logInfo("Uploading cache...");
-  const file = fs.createReadStream(filePath, {
-    fd: fs.openSync(filePath, "r"),
-    autoClose: false,
-    start: 0,
-    end: fileSize,
-  });
-  await uploadCache(cacheId, file, fileSize);
-  logInfo("Cache uploaded");
-
-  logInfo("Commiting cache...");
-  await commitCache(cacheId, fileSize);
-  logInfo("Cache committed");
+  await saveCache(key, version, filePath);
+  logInfo("Cache successfully saved");
 } catch (err) {
   logError(err);
   process.exit(1);

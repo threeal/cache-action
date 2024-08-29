3793c4534a5a3344705b2bcf1f1207e6297dd741
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-08-29 16:42:14 +0700

    refactor: rename action file to `main.mjs`

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "src/index.ts",
+  input: "src/main.ts",
   output: {
     dir: "dist",
     entryFileNames: "[name].mjs",

160a9468e71729a6b7aada14bfb0e270a52b44dd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-05 19:39:25 +0700

    feat: get file size before downloading files (#109)
    * feat: add `getDownloadFileSize` function
    
    * feat: modify `downloadFile` function to get file size before downloading

--- src/api/download.test.ts
+++ src/api/download.test.ts
@@ -1,13 +1,33 @@
 import { jest } from "@jest/globals";
 
+class Request {
+  url: string;
+  method: string;
+  headers: Record<string, string | undefined>;
+
+  constructor(url: string, method: string) {
+    this.url = url;
+    this.method = method;
+    this.headers = {};
+  }
+
+  setHeader(key: string, value: string): void {
+    this.headers[key] = value;
+  }
+}
+
 interface Response {
   headers: Record<string, string | undefined>;
   statusCode: number;
   data: () => string;
 }
 
-let clouds: Partial<Record<string, string>> = {};
+let clouds: Partial<Record<string, any>> = {};
 let files: Partial<Record<string, string>> = {};
+beforeEach(() => {
+  clouds = {};
+  files = {};
+});
 
 jest.unstable_mockModule("node:fs", () => ({
   default: {
@@ -19,7 +39,8 @@ jest.unstable_mockModule("node:fs", () => ({
 
 jest.unstable_mockModule("node:https", () => ({
   default: {
-    request: (url: string) => () => url,
+    request: (url: string, { method }: any): Request =>
+      new Request(url, method),
   },
 }));
 
@@ -37,25 +58,61 @@ jest.unstable_mockModule("./https.js", () => ({
     expect(res.headers["content-type"]).toContain(expectedType);
   },
   handleErrorResponse: async (res: Response) => new Error(res.data()),
-  sendRequest: async (req: () => string): Promise<Response> => {
-    const data = clouds[req()];
-    return data !== undefined
-      ? {
+  handleResponse: async (res: Response) => res.data(),
+  sendRequest: async (req: Request): Promise<Response> => {
+    const data = clouds[req.url];
+    if (data === undefined) {
+      return { statusCode: 404, headers: {}, data: () => "not found" };
+    }
+
+    switch (req.method) {
+      case "HEAD":
+        return {
           statusCode: 200,
-          headers: { "content-type": "application/octet-stream" },
-          data: () => data,
+          headers: { "content-length": data.length.toString() },
+          data: () => "",
+        };
+
+      case "GET":
+        if (typeof data === "string") {
+          const range = req.headers["range"]?.match(/bytes=(.*)-(.*)/s);
+          const start = range != null ? parseInt(range[1]) : 0;
+          const end = range != null ? parseInt(range[2]) : 0;
+          return {
+            statusCode: 206,
+            headers: {
+              "content-type": "application/octet-stream",
+              "content-length": data.length.toString(),
+            },
+            data: () => data.substring(start, end),
+          };
         }
-      : { statusCode: 404, headers: {}, data: () => "not found" };
+    }
+
+    return { statusCode: 500, headers: {}, data: () => "something went wrong" };
   },
 }));
 
-describe("download files", () => {
-  beforeEach(() => {
-    clouds = {};
-    files = {};
+describe("get the size of files to be downloaded", () => {
+  it("should return the size of a file to be downloaded", async () => {
+    const { getDownloadFileSize } = await import("./download.js");
+
+    clouds["a-url"] = "a content";
+
+    const fileSize = await getDownloadFileSize("a-url");
+    expect(fileSize).toBe(clouds["a-url"].length);
   });
 
-  it("it should download a file", async () => {
+  it("should throw an error for an invalid URL", async () => {
+    const { getDownloadFileSize } = await import("./download.js");
+
+    const prom = getDownloadFileSize("an-invalid-url");
+    await expect(prom).rejects.toThrow("not found");
+  });
+});
+
+describe("download files", () => {
+  it("should download a file successfully", async () => {
     const { downloadFile } = await import("./download.js");
 
     clouds["a-url"] = "a content";
@@ -65,10 +122,12 @@ describe("download files", () => {
     expect(files).toEqual({ "a-file": "a content" });
   });
 
-  it("should fail to download a file", async () => {
+  it("should throw an error because of invalid content", async () => {
     const { downloadFile } = await import("./download.js");
 
-    const prom = downloadFile("an-invalid-url", "a-file");
-    await expect(prom).rejects.toThrow("not found");
+    clouds["a-url"] = { length: 8 };
+
+    const prom = downloadFile("a-url", "a-file");
+    await expect(prom).rejects.toThrow("something went wrong");
   });
 });
--- src/api/download.ts
+++ src/api/download.ts
@@ -1,11 +1,34 @@
 import fs from "node:fs";
 import https from "node:https";
 import streamPromises from "node:stream/promises";
+
 import {
   assertResponseContentType,
   handleErrorResponse,
+  handleResponse,
   sendRequest,
 } from "./https.js";
+
+/**
+ * Retrieves the file size of a file to be downloaded from the specified URL.
+ *
+ * @param url - The URL of the file to be downloaded.
+ * @returns A promise that resolves to the size of the file to be downloaded, in bytes.
+ */
+export async function getDownloadFileSize(url: string): Promise<number> {
+  const req = https.request(url, { method: "HEAD" });
+  const res = await sendRequest(req);
+
+  switch (res.statusCode) {
+    case 200: {
+      await handleResponse(res);
+      return Number.parseInt(res.headers["content-length"] as string);
+    }
+
+    default:
+      throw await handleErrorResponse(res);
+  }
+}
 
 /**
  * Downloads a file from the specified URL and saves it to the provided path.
@@ -18,11 +41,15 @@ export async function downloadFile(
   url: string,
   savePath: string,
 ): Promise<void> {
-  const req = https.request(url);
+  const fileSize = await getDownloadFileSize(url);
+
+  const req = https.request(url, { method: "GET" });
+  req.setHeader("range", `bytes=0-${fileSize}`);
+
   const res = await sendRequest(req);
 
   switch (res.statusCode) {
-    case 200: {
+    case 206: {
       assertResponseContentType(res, "application/octet-stream");
       const file = fs.createWriteStream(savePath);
       await streamPromises.pipeline(res, file);

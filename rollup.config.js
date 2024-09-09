687eac3e643b333247e8c63b6d00996e3ffba5fc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-09 18:42:28 +0700

    feat: export cache functions (#86)
    * feat: add library entrypoint script
    
    * build: modify package to be public
    
    * build: modify gha-utils to be dev dependencies
    
    * build: enable TypeScript type declaration generation
    
    * ci: modify build package to create archive and upload as artifact

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,30 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import ts from "rollup-plugin-ts";
 
-export default ["src/main.ts", "src/post.ts"].map((input) => ({
-  input,
-  output: {
-    dir: "dist",
-    entryFileNames: "[name].mjs",
+const output = {
+  dir: "dist",
+  entryFileNames: "[name].mjs",
+};
+
+export default [
+  {
+    input: "src/lib.ts",
+    output,
+    plugins: [
+      ts({
+        tsconfig: (config) => ({ ...config, declaration: true }),
+        transpileOnly: true,
+      }),
+    ],
   },
-  plugins: [nodeResolve(), ts({ transpileOnly: true })],
-}));
+  {
+    input: "src/main.ts",
+    output,
+    plugins: [nodeResolve(), ts({ transpileOnly: true })],
+  },
+  {
+    input: "src/post.ts",
+    output,
+    plugins: [nodeResolve(), ts({ transpileOnly: true })],
+  },
+];

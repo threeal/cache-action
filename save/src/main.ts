3ae038c6115f5526bfed58b7729e532158835859
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-09-25 19:11:35 +0700

    feat: add `save` sub-action
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ save/src/main.ts
@@ -0,0 +1,22 @@
+import { getInput, logError, logInfo, setOutput } from "gha-utils";
+import { saveCache } from "../../src/lib.js";
+
+try {
+  const key = getInput("key");
+  const version = getInput("version");
+  const filePaths = getInput("files")
+    .split(/\s+/)
+    .filter((arg) => arg != "");
+
+  logInfo("Saving cache...");
+  if (await saveCache(key, version, filePaths)) {
+    logInfo("Cache successfully saved");
+    await setOutput("saved", "true");
+  } else {
+    logInfo("Aborting cache save, cache already exists");
+    await setOutput("saved", "false");
+  }
+} catch (err) {
+  logError(err);
+  process.exit(1);
+}
